2669a2f078886025827eba9b8d021c87a85fd2e4
rogger andré valverde flores <rogger.valverde@uni.pe>
2021-08-01 23:05:38 -0500

    feat(appointment-picker): transform to functional component

--- src/AppointmentPicker/AppointmentPicker.tsx
+++ src/AppointmentPicker/AppointmentPicker.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef } from 'react';
 import { Day } from './Day';
 import { Appointment } from './Appointment';
 import { Blank } from './Blank';
@@ -40,15 +40,6 @@ type RemoveAppointmentType = (
   removeCb: RemoveCallbackType
 ) => void;
 
-interface DefaultPropsInterface {
-  addAppointmentCallback: AddAppointmentType;
-  removeAppointmentCallback: RemoveAppointmentType;
-  maxReservableAppointments: number;
-  initialDay: Date;
-  unitTime: number;
-  local: string;
-}
-
 export type AppointmentAttributesType = {
   id?: IdentifierType;
   number: IdentifierType;
@@ -82,208 +73,194 @@ type SelectedAppointmentMapType = Map<
   Map<IdentifierType, SelectedAppointmentInterface>
 >;
 
-interface AppointmentPickerStateInterface {
-  selectedAppointments: SelectedAppointmentMapType;
-  size: number;
-  dayPeriods: number[];
-  dayLength: number;
+interface getAlreadySelectedAppointmentsInterface {
+  alpha?: boolean;
+  selectedByDefault?: boolean;
+  maxReservableAppointments?: number;
+  initialDay?: Date;
+  unitTime?: number;
+  local?: string;
+  days?: AppointmentAttributesType[][];
 }
 
-export class AppointmentPicker extends Component<
-  AppointmentPickerPropsInterface,
-  AppointmentPickerStateInterface
-> {
-  static defaultProps: DefaultPropsInterface = {
-    addAppointmentCallback: ({
-      addedAppointment: { day, number, time, id },
-      addCb
-    }) => {
-      console.log(
-        `Added appointment ${number}, day ${day}, time ${time}, id ${id}`
+const getAlreadySelectedAppointments = ({
+  maxReservableAppointments,
+  days,
+  alpha,
+  selectedByDefault,
+  initialDay,
+  unitTime,
+  local
+}: getAlreadySelectedAppointmentsInterface) => {
+  const selectedAppointments = new Map();
+  let size = 0;
+  if (selectedByDefault) {
+    days.forEach((day, index) => {
+      const actualDay = new Date(
+        initialDay.getTime() + 60 * 60 * 24 * 1000 * index
       );
-      addCb(day, number, time, id);
-    },
-    removeAppointmentCallback: ({ day, number, time, id }, removeCb) => {
-      console.log(
-        `Removed appointment ${number}, day ${day}, time ${time}, id ${id}`
-      );
-      removeCb(day, number);
-    },
-    maxReservableAppointments: 0,
-    initialDay: new Date(),
-    unitTime: 15 * 60 * 1000,
-    local: 'en-US'
-  };
+      const dayNumber = alpha
+        ? actualDay.toLocaleDateString(local, { weekday: 'long' })
+        : actualDay.toLocaleDateString(local);
 
-  constructor(props: AppointmentPickerPropsInterface) {
-    super(props);
-    const { days } = props;
-    const {
-      selectedAppointments,
-      size
-    } = this.getAlreadySelectedAppointments();
-    const dayPeriods = days.map((day) => {
-      let periods = 0;
-      day.forEach((obj) => {
-        periods = obj
-          ? obj.periods
-            ? periods + obj.periods
-            : periods + 1
-          : periods + 1;
-      });
-      return periods;
-    });
-    this.state = {
-      selectedAppointments: selectedAppointments,
-      size: size,
-      dayPeriods,
-      dayLength: Math.max.apply(null, dayPeriods)
-    };
-  }
-
-  static getDerivedStateFromProps(
-    props: AppointmentPickerPropsInterface,
-    state: AppointmentPickerStateInterface
-  ) {
-    const { selectedAppointments: currentSelectedAppointments } = state;
-    if (props.maxReservableAppointments < state.size) {
-      let sum = 0;
-      const selectedAppointments = new Map();
-      for (const currentDay of currentSelectedAppointments.keys()) {
-        const day = currentSelectedAppointments.get(currentDay);
-        const lengthByDay = day ? day.size : 0;
-        if (sum + lengthByDay < props.maxReservableAppointments) {
-          selectedAppointments.set(currentDay, day);
-        } else {
-          const dif = props.maxReservableAppointments - sum;
-          let i = 0;
-          if (day) {
-            for (const currentNumber of day.keys()) {
-              if (i < dif) day.delete(currentNumber);
-              i++;
-            }
-          }
-          selectedAppointments.set(currentDay, day);
-          return {
-            selectedAppointments: selectedAppointments,
-            size: props.maxReservableAppointments
-          };
-        }
-        sum = sum + lengthByDay;
-      }
-    }
-    return null;
-  }
-
-  shouldComponentUpdate(
-    nextProps: AppointmentPickerPropsInterface,
-    nextState: AppointmentPickerStateInterface
-  ) {
-    return (
-      nextState.selectedAppointments !== this.state.selectedAppointments ||
-      this.props.loading !== nextProps.loading
-    );
-  }
-
-  getAlreadySelectedAppointments = () => {
-    const selectedAppointments = new Map();
-    let size = 0;
-    const {
-      maxReservableAppointments,
-      alpha,
-      selectedByDefault,
-      initialDay,
-      unitTime,
-      local
-    } = this.props;
-    if (selectedByDefault) {
-      this.props.days.forEach((day, index) => {
-        const actualDay = new Date(
-          initialDay.getTime() + 60 * 60 * 24 * 1000 * index
-        );
-        const dayNumber = alpha
-          ? actualDay.toLocaleDateString(local, { weekday: 'long' })
-          : actualDay.toLocaleDateString(local);
-
-        let key = 0;
-        day.forEach((appointment, _) => {
-          if (appointment === null) {
-            key = key + 1;
-          } else if (appointment.isSelected) {
-            const time = new Date(
-              actualDay.getTime() + unitTime * key
-            ).toLocaleTimeString(local);
-            const appointmentAlreadySelected = this.includeAppointment(
+      let key = 0;
+      day.forEach((appointment, _) => {
+        if (appointment === null) {
+          key = key + 1;
+        } else if (appointment.isSelected) {
+          const time = new Date(
+            actualDay.getTime() + unitTime * key
+          ).toLocaleTimeString(local);
+          const appointmentAlreadySelected = includeAppointment(
+            selectedAppointments,
+            dayNumber,
+            appointment.number
+          );
+          if (size < maxReservableAppointments && !appointmentAlreadySelected) {
+            addAppointment(
               selectedAppointments,
               dayNumber,
-              appointment.number
+              appointment.number,
+              time,
+              appointment.id
             );
-            if (
-              size < maxReservableAppointments &&
-              !appointmentAlreadySelected
-            ) {
-              this.addAppointment(
-                selectedAppointments,
-                dayNumber,
-                appointment.number,
-                time,
-                appointment.id
-              );
-              size = size + 1;
-            }
+            size = size + 1;
           }
-        });
+        }
+      });
+    });
+  }
+  return { selectedAppointments, size };
+};
+
+const includeAppointment = (
+  selectedAppointments: SelectedAppointmentMapType,
+  day: string,
+  number: IdentifierType
+) => {
+  const currentDay = selectedAppointments.get(day);
+  if (currentDay) {
+    return !!currentDay.get(number);
+  }
+  return false;
+};
+
+const addAppointment = (
+  selectedAppointments: SelectedAppointmentMapType,
+  day: string,
+  number: IdentifierType,
+  time: string,
+  id?: IdentifierType
+) => {
+  const currentDay = selectedAppointments.get(day);
+  if (currentDay) {
+    const currentAppointment = currentDay.get(number);
+    if (!currentAppointment) {
+      currentDay.set(number, {
+        id,
+        time
       });
     }
-    return { selectedAppointments, size };
-  };
+  } else {
+    selectedAppointments.set(
+      day,
+      new Map([
+        [
+          number,
+          {
+            id,
+            time
+          }
+        ]
+      ])
+    );
+  }
+};
 
-  includeAppointment = (
-    selectedAppointments: SelectedAppointmentMapType,
-    day: string,
-    number: IdentifierType
-  ) => {
-    const currentDay = selectedAppointments.get(day);
-    if (currentDay) {
-      return !!currentDay.get(number);
-    }
-    return false;
-  };
+const AppointmentPicker = ({
+  alpha,
+  selectedByDefault,
+  continuous,
+  loading,
+  addAppointmentCallback = ({
+    addedAppointment: { day, number, time, id },
+    addCb
+  }) => {
+    console.log(
+      `Added appointment ${number}, day ${day}, time ${time}, id ${id}`
+    );
+    addCb(day, number, time, id);
+  },
+  removeAppointmentCallback = ({ day, number, time, id }, removeCb) => {
+    console.log(
+      `Removed appointment ${number}, day ${day}, time ${time}, id ${id}`
+    );
+    removeCb(day, number);
+  },
+  days,
+  maxReservableAppointments = 0,
+  initialDay = new Date(),
+  unitTime = 15 * 60 * 1000,
+  visible,
+  local = 'en-US'
+}: AppointmentPickerPropsInterface) => {
+  const {
+    selectedAppointments: alreadySelectedAppointments,
+    size: actualSize
+  } = getAlreadySelectedAppointments({
+    maxReservableAppointments,
+    days,
+    alpha,
+    selectedByDefault,
+    initialDay,
+    unitTime,
+    local
+  });
+  const actualDayPeriods = days.map((day) => {
+    let periods = 0;
+    day.forEach((obj) => {
+      periods = obj
+        ? obj.periods
+          ? periods + obj.periods
+          : periods + 1
+        : periods + 1;
+    });
+    return periods;
+  });
+  const [selectedAppointments, setSelectedAppointments] = useState(
+    alreadySelectedAppointments
+  );
+  const size = useRef(actualSize);
+  const [, setSize] = useState(actualSize);
+  const [dayPeriods] = useState(actualDayPeriods);
+  const [dayLength] = useState(Math.max.apply(null, dayPeriods));
 
-  addAppointment = (
-    selectedAppointments: SelectedAppointmentMapType,
-    day: string,
-    number: IdentifierType,
-    time: string,
-    id?: IdentifierType
-  ) => {
-    const currentDay = selectedAppointments.get(day);
-    if (currentDay) {
-      const currentAppointment = currentDay.get(number);
-      if (!currentAppointment) {
-        currentDay.set(number, {
-          id,
-          time
-        });
+  if (maxReservableAppointments < size.current) {
+    let sum = 0;
+    const newSelectedAppointments = new Map();
+    for (const currentDay of selectedAppointments.keys()) {
+      const day = selectedAppointments.get(currentDay);
+      const lengthByDay = day ? day.size : 0;
+      if (sum + lengthByDay < maxReservableAppointments) {
+        newSelectedAppointments.set(currentDay, day);
+      } else {
+        const dif = maxReservableAppointments - sum;
+        let i = 0;
+        if (day) {
+          for (const currentNumber of day.keys()) {
+            if (i < dif) day.delete(currentNumber);
+            i++;
+          }
+        }
+        newSelectedAppointments.set(currentDay, day);
+        setSelectedAppointments(newSelectedAppointments);
+        setSize(maxReservableAppointments);
       }
-    } else {
-      selectedAppointments.set(
-        day,
-        new Map([
-          [
-            number,
-            {
-              id,
-              time
-            }
-          ]
-        ])
-      );
+      sum = sum + lengthByDay;
     }
-  };
+  }
 
-  deleteAppointment = (day: string, number: IdentifierType) => {
-    const { selectedAppointments } = this.state;
+  const deleteAppointment = (day: string, number: IdentifierType) => {
     const currentDay = selectedAppointments.get(day);
     if (currentDay) {
       currentDay.delete(number);
@@ -293,70 +270,50 @@ export class AppointmentPicker extends Component<
     }
   };
 
-  acceptSelection = (
+  const acceptSelection = (
     day: string,
     number: IdentifierType,
     time: string,
     id?: IdentifierType
   ) => {
-    const { selectedAppointments, size } = this.state;
-    const { maxReservableAppointments } = this.props;
-    if (size < maxReservableAppointments) {
-      this.addAppointment(selectedAppointments, day, number, time, id);
-      this.setState({
-        size: size + 1
-      });
+    if (size.current < maxReservableAppointments) {
+      addAppointment(selectedAppointments, day, number, time, id);
+      size.current = size.current + 1;
+      setSize((currentSize) => currentSize + 1);
     }
   };
 
-  acceptDeselection = (day: string, number: IdentifierType) => {
-    const size = this.state.size;
-
-    this.deleteAppointment(day, number);
-    this.setState({
-      size: size - 1
-    });
+  const acceptDeselection = (day: string, number: IdentifierType) => {
+    deleteAppointment(day, number);
+    size.current = size.current - 1;
+    setSize((currentSize) => currentSize - 1);
   };
 
-  selectAppointment = (
+  const selectAppointment = (
     day: string,
     number: IdentifierType,
     time: string,
     id?: IdentifierType
   ) => {
-    const { selectedAppointments } = this.state;
-    const size = this.state.size;
-    const {
-      maxReservableAppointments,
-      addAppointmentCallback,
-      removeAppointmentCallback,
-      continuous
-    } = this.props;
-    const appointmentAlreadySelected = this.includeAppointment(
+    const appointmentAlreadySelected = includeAppointment(
       selectedAppointments,
       day,
       number
     );
 
-    if (size < maxReservableAppointments) {
+    if (size.current < maxReservableAppointments) {
       if (!appointmentAlreadySelected) {
         addAppointmentCallback({
           addedAppointment: { day, number, time, id },
-          addCb: this.acceptSelection
+          addCb: acceptSelection
         });
       } else {
-        removeAppointmentCallback(
-          { day, number, time, id },
-          this.acceptDeselection
-        );
+        removeAppointmentCallback({ day, number, time, id }, acceptDeselection);
       }
     } else {
       const currentDay = selectedAppointments.get(day);
       if (currentDay && appointmentAlreadySelected) {
-        removeAppointmentCallback(
-          { day, number, time, id },
-          this.acceptDeselection
-        );
+        removeAppointmentCallback({ day, number, time, id }, acceptDeselection);
       } else if (continuous) {
         const auxDay = selectedAppointments.keys().next().value;
         const auxDayInstance = selectedAppointments.get(auxDay);
@@ -364,32 +321,21 @@ export class AppointmentPicker extends Component<
         const auxNumberInstance = auxDayInstance.get(auxNumber);
         addAppointmentCallback({
           addedAppointment: { day, number, time, id },
-          addCb: this.acceptSelection,
+          addCb: acceptSelection,
           removedAppointment: {
             day: auxDay,
             number: auxNumber,
             time: auxNumberInstance.time,
             id: auxNumberInstance.id
           },
-          removeCb: this.acceptDeselection
+          removeCb: acceptDeselection
         });
       }
     }
   };
 
-  render() {
-    return (
-      <div className='appointment-content'>
-        <div className={this.props.loading ? 'loader' : undefined} />
-        <div className='appointment-picker'>{this.renderDays()}</div>
-      </div>
-    );
-  }
-
-  renderDays() {
-    const { selectedAppointments: appointments, dayPeriods } = this.state;
-    const { alpha, visible, initialDay, local } = this.props;
-    return this.props.days.map((day, index) => {
+  const renderDays = () => {
+    return days.map((day, index) => {
       const actualDay = new Date(
         initialDay.getTime() + 60 * 60 * 24 * 1000 * index
       );
@@ -403,19 +349,19 @@ export class AppointmentPicker extends Component<
         ? actualDay.toLocaleDateString(local, { weekday: 'long' })
         : actualDay.toLocaleDateString(local);
 
-      const isSelected = !!appointments.get(dayNumber);
+      const isSelected = !!selectedAppointments.get(dayNumber);
       const props = {
         visible,
         dayNumber,
         isSelected,
         selectedAppointment: null,
         appointments: day,
-        selectAppointment: this.selectAppointment
+        selectAppointment
       };
 
       return (
         <Day key={index} {...props}>
-          {this.renderAppointments(
+          {renderAppointments(
             day,
             dayNumber,
             isSelected,
@@ -425,22 +371,15 @@ export class AppointmentPicker extends Component<
         </Day>
       );
     });
-  }
+  };
 
-  renderAppointments(
+  const renderAppointments = (
     appointments: AppointmentAttributesType[],
     dayNumber: string,
     isDaySelected: boolean,
     periods: number,
     actualDay: Date
-  ) {
-    const { selectedAppointments, size, dayLength } = this.state;
-    const {
-      maxReservableAppointments,
-      unitTime,
-      local,
-      continuous
-    } = this.props;
+  ) => {
     const blanks = new Array(
       dayLength - periods > 0 ? dayLength - periods : 0
     ).fill(0);
@@ -455,22 +394,19 @@ export class AppointmentPicker extends Component<
       ).toLocaleTimeString(local);
       const isSelected =
         isDaySelected &&
-        this.includeAppointment(
-          selectedAppointments,
-          dayNumber,
-          appointment.number
-        );
+        includeAppointment(selectedAppointments, dayNumber, appointment.number);
       const props = {
         isSelected,
         isReserved: appointment.isReserved,
-        isEnabled: size < maxReservableAppointments || continuous,
-        selectAppointment: this.selectAppointment.bind(
-          this,
-          dayNumber,
-          appointment.number,
-          time,
-          appointment.id
-        ),
+        isEnabled: size.current < maxReservableAppointments || continuous,
+        selectAppointment: () => {
+          selectAppointment(
+            dayNumber,
+            appointment.number,
+            time,
+            appointment.id
+          );
+        },
         appointmentNumber: time,
         periods: appointment.periods ? appointment.periods : 1,
         time: time
@@ -486,5 +422,14 @@ export class AppointmentPicker extends Component<
       });
     }
     return day;
-  }
-}
+  };
+
+  return (
+    <div className='appointment-content'>
+      <div className={loading ? 'loader' : undefined} />
+      <div className='appointment-picker'>{renderDays()}</div>
+    </div>
+  );
+};
+
+export { AppointmentPicker };
